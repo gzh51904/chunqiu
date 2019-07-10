503f555385d77fcef970f5dc62189bd70672d5c7
peter <q1871508593@foxmail.com>
2019-07-10 17:34:37 +0800

    rootmenu

--- SpringTour/chunqiulvyou/src/main.js
+++ SpringTour/chunqiulvyou/src/main.js
@@ -1,8 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from "./routers"
+import MintUI from 'mint-ui'  //npm i mint-ui -S
+import 'mint-ui/lib/style.css'
+import axios from "axios"     //npm i axios
+
+
+Vue.use(MintUI) // npm i babel-plugin-component
+Vue.use(router)
+Vue.$axios = axios
+
 
 Vue.config.productionTip = false
 
+
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
